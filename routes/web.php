d5324eeebb4493e0361ea53238f367567b7c2ff8
HON <asp.1029.qazplm@gmail.com>
2026-07-20 20:38:59 +0900

    feat. ログイン、会員登録調整

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
-
 // ==========================================
 // 認証必須ページ (ログインが必要)
 // ==========================================
